9aee96ef523754c1b59d08e8083419a4f5703bcd
polaris <61411085+polaris428@users.noreply.github.com>
2025-06-04 17:18:28 +0900

    🔥안쓰는 파일 삭제

--- src/app/api/folders/[id]/invite/route.ts
+++ src/app/api/folders/[id]/invite/route.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import '@/infrastructure/di/container';
 import { NextRequest, NextResponse } from 'next/server';
 import { container } from 'tsyringe';
-import { GenerateInviteCode } from '@/application/usecases/folder/join/GenerateInviteCodeUsecase';
+import { GenerateInviteCodeUsecase } from '@/application/usecases/folder/join/GenerateInviteCodeUsecase';
 import { getCurrentUserId } from '@/lib/utils/getCurrentUserId';
 import { getVerifiedUser } from '@/lib/utils/getVerifiedUser';
 export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
@@ -12,7 +12,7 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id
     const folderId = (await params).id;
     const body = await req.json();
 
-    const usecase = container.resolve(GenerateInviteCode);
+    const usecase = container.resolve(GenerateInviteCodeUsecase);
     const inviteCode = await usecase.execute(folderId, body.isInvite, userId);
 
     return NextResponse.json({ inviteCode });
