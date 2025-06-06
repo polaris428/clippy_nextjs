4ae0e285ae02b30ee58fa106f3e7bc65924eef75
polaris <61411085+polaris428@users.noreply.github.com>
2025-06-06 19:13:55 +0900

    ♻️오타 수정

--- src/app/api/links/[id]/route.ts
+++ src/app/api/links/[id]/route.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import '@/infrastructure/di/container';
 import { DELETE as deleteHandler } from '@/presentation/routes/links/delete';
-import { PATCH as pathHandler } from '@/presentation/routes/links/useUpdateLink';
+import { PATCH as patchHandler } from '@/presentation/routes/links/useUpdateLink';
 
 export const DELETE = deleteHandler;
 
-export const PATCH = pathHandler;
+export const PATCH = patchHandler;
--- src/hooks/user/useCreateLink.ts
+++ src/hooks/user/useCreateLink.ts
@@ -25,22 +25,16 @@ export function useCreateLink() {
       isPin: false,
     };
 
-    // 1️⃣ 더미 추가
     addLink(input.folderId, dummyLink);
 
     try {
-      // 2️⃣ 서버로 전송
-      console.log('서버로 전송1');
       const realLink = await LinkService.createLink(input);
-      console.log(realLink);
-      // 3️⃣ 응답 온 링크로 갱신
+
       updateLink(input.folderId, tempId, realLink);
       console.log('서버로 전송3');
       return true;
     } catch (err) {
       console.error('🔥 링크 생성 실패:', err);
-
-      // 4️⃣ 실패한 링크 표시
       updateLink(input.folderId, tempId, {
         ...dummyLink,
         title: '[저장 실패]',
