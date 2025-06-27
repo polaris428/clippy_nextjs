338863d1fd4120d9efdf4690d10a661eed248f4c
polaris <61411085+polaris428@users.noreply.github.com>
2025-06-27 22:14:40 +0900

    ✨파이어베이스 어드민 env에서 관리하도록 변경

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,21 @@
 import * as admin from 'firebase-admin';
-import * as fs from 'fs';
-import * as path from 'path';
 import logger from './logger/logger';
 
-const serviceAccountPath = path.resolve(process.cwd(), 'firebase-admin.json');
-
 try {
-  const raw = fs.readFileSync(serviceAccountPath, 'utf8');
-  const serviceAccount = JSON.parse(raw);
+  const credentialJson = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
-  if (!admin.apps.length && serviceAccount) {
+  if (!credentialJson) throw new Error('FIREBASE_ADMIN_CREDENTIAL is not defined');
+
+  const serviceAccount = JSON.parse(credentialJson as string);
+  if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
   }
 } catch (err) {
-  logger.error({ err }, '🔥 Failed to load firebase-admin.json:');
+  logger.error({ err }, '🔥 Failed to initialize firebase-admin with env credential:');
 }
+
 export const verifyIdToken = (token: string) => {
   return admin.auth().verifyIdToken(token);
 };
